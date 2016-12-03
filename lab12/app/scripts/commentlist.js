bc5c75886a1a369e4ddbaf5c54f789af8ee1928a
Daniel <dtg5@students.calvin.edu>
2016-12-03 04:09:11 -0500

    Hope it works this time.

--- lab12/app/scripts/commentlist.js
+++ lab12/app/scripts/commentlist.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import $ from 'jquery';
+import Remarkable from 'remarkable';
 
 import Comment from './comment';
 
-// COMMENT LIST
 module.exports = React.createClass({
-  render: function() {
-    var commentNodes = this.props.data.map(function(comment) {
-      return (
-        <Comment author={comment.author} key={comment.id}>
-          {comment.text}
-        </Comment>
-      );
-    });
-    return (
-      <div className="commentList">
-        {commentNodes}
-      </div>
-    );
-  }
+    render: function() {
+        var commentNodes = this.props.data.map(function(comment) {
+            return (
+                <Comment id={comment.id} author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        );
+    }
 });
