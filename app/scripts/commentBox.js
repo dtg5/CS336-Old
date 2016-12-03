96964a4339935b1e38e8d0270af4587f15bc3bef
Daniel <dtg5@students.calvin.edu>
2016-12-03 15:14:26 -0500

    Almost done

---
+++ app/scripts/commentBox.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import CommentList from './commentList';
+import CommentForm from './commentForm';
+
+import { store, ActionTools } from './flux'; 
+
+module.exports = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    handleCommentSubmit: function(comment) {
+        var comments = this.state.data;
+        comment.id = Date.now();
+        var newComments = comments.concat([comment]);
+        this.setState({data: newComments});
+        store.dispatch(ActionTools.addComment(comment));
+        .done(function(result){
+            this.setState({data: result});
+        }.bind(this))
+        .fail(function(xhr, status, errorThrown) {
+            this.setState({data: comments});
+            console.error(API_URL, status, errorThrown.toString());
+        }.bind(this));
+    },
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+            </div>
+        );
+    }
+});
+
+componentWillMount() {
+    this.unsubscribe = store.subscribe(() => {
+        this.setState({
+            data: store.getState().data
+        });
+    });
+},
+componentWillUnmount: function() {
+    this.unsubscribe();
+},
