18baffc2bbdc377866b11eb252cf0623e425fc1d
lct6 <lct6@students.calvin.edu>
2016-12-19 12:16:05 -0500

    spelled out VGS

--- app/scripts/start.js
+++ app/scripts/start.js
@@ -9,7 +9,7 @@ export default React.createClass({
     return (
       <div>
         <img src="../css/logo.png"/>
-        <h1>VGS Website</h1>
+        <h1>Video Game Sphere Website</h1>
         <ul role="nav">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
